6756031919247fa9dff77d6753b0299c811bb27a
Julian Peroncini <julianperoncini@gmail.com>
2024-12-17 11:40:12 -0300

    fixes qa

--- components/SiteHead.vue
+++ components/SiteHead.vue
@@ -14,7 +14,18 @@
                     </li>
 
                     <li class="relative">
-                        <nuxt-link class="uline relative mont font-medium text-16 leading-none tracking-[-0.64px] w-full h-full" to="/asesor-inteligente">Cultivos</nuxt-link>
+                        <div class="cursor-pointer flex items-center relative mont font-medium text-16 leading-none tracking-[-0.64px] w-full h-full">
+                            <span class="inline-block mr-8">
+                                Cultivos
+                            </span>
+                            <span class="inline-block">
+                                <svg class="w-[1rem] h-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
+                                    <g transform="translate(8) rotate(90)">
+                                        <path fill="#3FADE1" id="Shape" class="cls-2" d="M10,1A1,1,0,0,0,8.293.293L5,3.586,1.707.293A1,1,0,1,0,.293,1.707l4,4a1,1,0,0,0,1.415,0l4-4A1,1,0,0,0,10,1Z" transform="translate(2 8) rotate(-90)"/>
+                                    </g>
+                                </svg>
+                            </span>
+                        </div>
                     </li>
 
                     <li class="relative">
--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,147 +1,241 @@
 <template>
-    <div class="relative w-full h-screen">
-      <div ref="sliderContainer" class="slider-container h-full">
-        <div 
-          v-for="(slide, index) in slides" 
-          :key="index"
-          class="slide absolute w-full h-full"
-          :class="{ active: currentSlide === index }"
-        >
-          <figure class="relative w-full h-full">
-            <img 
-              :src="slide.image" 
-              :alt="slide.alt"
-              class="relative w-full h-full object-cover"
-            >
-          </figure>
-  
-          <div class="px-20 s:px-0 w-full s:w-[inherit] flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
-            <h1 class="mont text-center s:text-left font-semibold text-50 leading-none tracking-[-2px] js-t-fades">
-              {{ slide.title }} <strong class="mont font-extrabold">{{ slide.strongText }}</strong>
-            </h1>
-  
-            <div class="relative mt-40 js-t-fades">
-              <ButtonArrow />
-            </div>
+  <div class="relative w-full h-screen">
+    <div ref="sliderContainer" class="slider-container h-full">
+      <div 
+        v-for="(slide, index) in slides" 
+        :key="index"
+        class="slide absolute w-full h-full"
+        :class="{ active: currentSlide === index }"
+      >
+        <figure class="relative w-full h-full">
+          <img 
+            :src="slide.image" 
+            :alt="slide.alt"
+            class="relative w-full h-full object-cover"
+          >
+        </figure>
+
+        <div class="px-20 s:px-0 w-full s:w-[inherit] flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
+          <h1 class="mont text-center s:text-left font-semibold text-50 leading-none tracking-[-2px] js-t-fades">
+            {{ slide.title }} 
+            <strong class="mont font-extrabold relative overflow-hidden">
+              <div class="word-container">
+                <span 
+                  v-if="isFirstSlideRotation" 
+                  class="word-animation"
+                  :class="{ 'animate-out': wordChanged }"
+                >
+                  {{ rotatingWords[currentRotatingWord] }}
+                </span>
+                <span v-else>{{ slide.strongText }}</span>
+              </div>
+            </strong>
+          </h1>
+
+          <div class="relative mt-40 js-t-fades">
+            <ButtonArrow />
           </div>
         </div>
       </div>
-  
-      <!-- Custom Dots -->
-      <div class="absolute bottom-20 left-1/2 transform -translate-x-1/2 flex gap-8 s:gap-4 z-20">
-        <button
-          v-for="(_, index) in slides"
-          :key="index"
-          @click="goToSlide(index)"
-          class="dot-button w-[1.2rem] h-[1.2rem] rounded-full transition-all duration-500"
-          :class="currentSlide === index ? 'dot-active' : 'dot-inactive'"
-          :aria-label="`Go to slide ${index + 1}`"
-        ></button>
-      </div>
     </div>
+
+    <!-- Custom Dots -->
+    <div class="absolute bottom-20 left-1/2 transform -translate-x-1/2 flex gap-8 s:gap-4 z-20 pointer-events-none z-[11]">
+      <button
+        v-for="(_, index) in slides"
+        :key="index"
+        @click="goToSlide(index)"
+        class="dot-button w-[1.2rem] h-[1.2rem] rounded-full transition-all duration-500 pointer-events-auto"
+        :class="currentSlide === index ? 'dot-active' : 'dot-inactive'"
+        :aria-label="`Go to slide ${index + 1}`"
+      ></button>
+    </div>
+  </div>
 </template>
-  
+
 <script>
-  export default {
-    data() {
-      return {
-        currentSlide: 0,
-        slides: [
-          {
-            image: 'hero-home.jpg',
-            alt: 'Bioceres',
-            title: 'Hagamos que crezca tu',
-            strongText: 'negocio'
-          },
-          {
-            image: 'hero-home.jpg',
-            alt: 'Bioceres',
-            title: 'Hagamos que crezca tu',
-            strongText: 'cultivo'
-          },
-          {
-            image: 'hero-home.jpg',
-            alt: 'Bioceres',
-            title: 'Hagamos que crezca tu',
-            strongText: 'rentabilidad'
-          },
-          {
-            image: 'hero-home.jpg',
-            alt: 'Bioceres',
-            title: 'Hagamos que crezca tu',
-            strongText: 'comunidad'
-          }
-        ],
-        autoplayInterval: null
+export default {
+data() {
+  return {
+    currentSlide: 0,
+    currentRotatingWord: 0,
+    isFirstSlideRotation: true,
+    wordChanged: false,
+    rotatingWords: ['negocio', 'cultivo', 'rentabilidad', 'comunidad'],
+    rotatingInterval: null,
+    slides: [
+      {
+        image: 'hero-home.jpg',
+        alt: 'Bioceres',
+        title: 'Hagamos que crezca tu',
+        strongText: 'negocio'
+      },
+      {
+        image: 'hero-home.jpg',
+        alt: 'Bioceres',
+        title: 'Hagamos que crezca tu',
+        strongText: 'cultivo'
+      },
+      {
+        image: 'hero-home.jpg',
+        alt: 'Bioceres',
+        title: 'Hagamos que crezca tu',
+        strongText: 'rentabilidad'
+      },
+      {
+        image: 'hero-home.jpg',
+        alt: 'Bioceres',
+        title: 'Hagamos que crezca tu',
+        strongText: 'comunidad'
       }
-    },
-
-    mounted() {
-      this.startAutoplay()
-    },
-
-    beforeDestroy() {
-      this.stopAutoplay()
-    },
-
-    methods: {
-      goToSlide(index) {
-        this.currentSlide = index
-        this.resetAutoplay()
-      },
-
-      nextSlide() {
-        this.currentSlide = (this.currentSlide + 1) % this.slides.length
-      },
-
-      startAutoplay() {
-        this.autoplayInterval = setInterval(() => {
-          this.nextSlide()
-        }, 3500)
-      },
-
-      stopAutoplay() {
-        if (this.autoplayInterval) {
-          clearInterval(this.autoplayInterval)
-        }
-      },
-
-      resetAutoplay() {
-        this.stopAutoplay()
-        this.startAutoplay()
-      }
-    }
+    ],
+    autoplayInterval: null
   }
+},
+
+mounted() {
+  this.startFirstSlideRotation()
+},
+
+beforeDestroy() {
+  this.stopAutoplay()
+  if (this.rotatingInterval) clearInterval(this.rotatingInterval)
+},
+
+methods: {
+  startFirstSlideRotation() {
+    let animationInProgress = false;
+    
+    this.rotatingInterval = setInterval(() => {
+      if (animationInProgress) return;
+      
+      animationInProgress = true;
+      this.wordChanged = true;
+      
+      setTimeout(() => {
+        this.currentRotatingWord = (this.currentRotatingWord + 1) % this.rotatingWords.length;
+        this.wordChanged = false;
+        animationInProgress = false;
+      }, 500);
+      
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(this.rotatingInterval);
+      this.isFirstSlideRotation = false;
+      this.startAutoplay();
+    }, 4500);
+  },
+
+  goToSlide(index) {
+    if (!this.isFirstSlideRotation) {
+      this.currentSlide = index
+      this.resetAutoplay()
+    }
+  },
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length
+  },
+
+  startAutoplay() {
+    this.autoplayInterval = setInterval(() => {
+      this.nextSlide()
+    }, 5500)
+  },
+
+  stopAutoplay() {
+    if (this.autoplayInterval) {
+      clearInterval(this.autoplayInterval)
+    }
+  },
+
+  resetAutoplay() {
+    this.stopAutoplay()
+    this.startAutoplay()
+  }
+}
+}
 </script>
-  
+
 <style scoped>
 .slide {
   opacity: 0;
   transition: opacity 0.5s ease-in-out;
+  pointer-events: none;
+  z-index: 1;
 }
 
 .slide.active {
   opacity: 1;
+  pointer-events: auto !important;
+  z-index: 10;
 }
 
 .dot-button {
-  border: none;
-  padding: 0;
-  cursor: pointer;
-  outline: none;
+border: none;
+padding: 0;
+cursor: pointer;
+outline: none;
 }
 
 .dot-active {
-  background-color: rgb(75, 169, 255);
-  transform: scale(1);
+background-color: rgb(75, 169, 255);
+transform: scale(1);
 }
 
 .dot-inactive {
-  background-color: rgba(128, 128, 128, 0.6);
-  transform: scale(0.85);
+background-color: rgba(128, 128, 128, 0.6);
+transform: scale(0.85);
 }
 
 .dot-inactive:hover {
-  background-color: rgba(128, 128, 128, 0.8);
+background-color: rgba(128, 128, 128, 0.8);
+}
+
+.word-container {
+position: relative;
+display: inline-block;
+min-width: 200px;
+}
+
+.word-animation:first-child {
+  position: relative !important;
+}
+
+.word-animation {
+display: inline-block;
+opacity: 1;
+transform: translateY(0);
+position: absolute;
+top: 0;
+left: 0;
+animation: slideInUp 0.5s cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.word-animation.animate-out {
+animation: slideOutUp 0.5s cubic-bezier(0.4, 0, 0.2, 1) forwards;
+}
+
+@keyframes slideOutUp {
+0% {
+  opacity: 1;
+  transform: translateY(0);
+}
+100% {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+}
+
+@keyframes slideInUp {
+0% {
+  opacity: 0;
+  transform: translateY(20px);
+}
+100% {
+  opacity: 1;
+  transform: translateY(0);
+}
 }
 </style>
